d7417e42f32f9c843cae25bdd77ed21b22188b90
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-03-03 22:40:28 +0100

    Added basic data structures - PoC structs.rs

---
+++ src/structs.rs
@@ -0,0 +1,56 @@
+
+enum DomainStates {
+    Ok,
+    Warning,
+    Suspected,
+    Malicious,
+    Hacked,
+    Unresolvable,
+    Broken,
+    Empty,
+    Unknown,
+}
+
+enum WebAppTypes {
+    WordPress,
+    Joomla,
+    Prestashop,
+    Phpbb,
+    Magento,
+    Moodle,
+    Owncloud,
+    Drupal,
+    Mybb,
+    CmsMadeSimple,
+    PhpFusion,
+    Zurmo,
+    Phpmyadmin,
+    Afterlogic,
+    Squirrelmail,
+    Roundcube,
+    Limesurvey,
+    ZenCart,
+    Piwik,
+    PHPList,
+    Mamboo,
+    CustomX,
+}
+
+enum Interpreters {
+    Php52,
+    Php53,
+    Php54,
+    Php55,
+    Php56,
+    Php70,
+    Python27,
+    Python35,
+    Perl,
+    Shell,
+    Text,
+}
+
+
+struct Changeset {
+
+}
