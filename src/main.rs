d2f60b0dcd7e4018c822ca992bbef4a53ce22af1
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-03-28 19:35:09 +0200

    Moved processors test code to process module. Done cleanup refactoring of main.

--- src/main.rs
+++ src/main.rs
@@ -24,26 +24,14 @@ extern crate bincode;
 extern crate rustc_serialize;
 // extern crate flame;
 // extern crate rsgenetic;
+
 // #[macro_use] extern crate nickel;
 // use nickel::Nickel;
-use std::os::unix::fs::MetadataExt; /* Metadata trait */
+
 use uuid::Uuid;
-use regex::Regex;
-use core::result::Result;
-use time::{get_time, precise_time_ns};
-use cld2::{detect_language, Format, Reliable, Lang};
-use walkdir::{WalkDir}; // DirEntry, WalkDirIterator
-use users::get_user_by_uid;
-use std::io::prelude::{Read,Write};
-use std::io::{BufReader, BufWriter};
-use std::fs::{File, OpenOptions};
+use time::precise_time_ns;
+use walkdir::WalkDir;
 use std::path::Path;
-use curl::http;
-use rustc_serialize::json;
-use flate2::Compression;
-use flate2::read::GzDecoder;
-use flate2::write::ZlibEncoder;
-use bincode::rustc_serialize::{encode, decode};
 
 // local
 mod structs;
@@ -113,44 +101,11 @@ fn main() {
         }
 
         /* write changeset serialized to json */
-        let output_file = format!("{}_{}.chgset.json", user.name(), changeset.uuid);
-        match OpenOptions::new()
-                            .read(true)
-                            .create(true)
-                            .write(true)
-                            .append(false)
-                            .open(output_file.clone()) {
-            Ok(f) => {
-                let mut writer = BufWriter::new(f);
-                match writer.write(changeset.to_string().as_bytes()) {
-                    Ok(some) => {
-                        debug!("Changeset: {} has been stored in: {} ({} bytes)", changeset.uuid, output_file, some)
-                    },
-                    Err(err) => {
-                        error!("Error: {}, file: {}", err, output_file)
-                    }
-                }
-            },
-            Err(err) => {
-                error!("File open error: {}, file: {}", err, output_file)
-            }
-        }
+        let (file_name, bytes_written) = store_changeset_json(user.name().to_string(), changeset.clone());
+        info!("Changeset(json) stored: {} ({} bytes)", file_name, bytes_written);
 
         /* now write compressed binary changeset */
-        let changeset_dir = format!(".changesets/{}", user.name());
-        match std::fs::create_dir_all(changeset_dir.clone()) {
-            Ok(_) => {},
-            Err(err) => error!("{:?}", err),
-        }
-        let file_name = format!("{}/{}-{}.chgset", changeset_dir, changeset.uuid, changeset.timestamp);
-        let binary_encoded = encode(&changeset, bincode::SizeLimit::Infinite).unwrap();
-
-        let mut zlib = ZlibEncoder::new(Vec::new(), Compression::Best);
-        zlib.write(&binary_encoded[..]).unwrap();
-        let compressed_bytes = zlib.finish().unwrap();
-
-        let mut writer = BufWriter::new(File::create(file_name.clone()).unwrap());
-        let bytes_written = writer.write(&compressed_bytes).unwrap();
+        let (file_name, bytes_written) = store_changeset(user.name().to_string(), changeset);
         info!("Changeset stored: {} ({} bytes)", file_name, bytes_written);
     }
 
@@ -177,58 +132,6 @@ fn fetch_users_test() {
         } else {
             assert!(user.uid() > 0);
             assert!(user.name().len() > 0);
-        }
-    }
-}
-
-
-#[cfg(test)]
-#[test]
-fn language_detection_test() {
-    let _ = env_logger::init();
-    let texts = vec!("Młody Amadeusz szedł suchą szosą.", "Mladý Amadeusz išiel suchej ceste.", "Young Amadeus went a dry road.");
-    let expected = vec!("pl", "sk", "en");
-    for (text, res) in texts.iter().zip(expected.iter()) {
-        match detect_language(text, Format::Text).0 { /* ignore detection reliability here */
-            Some(Lang(lang)) => assert!(lang.to_string() == res.to_string()),
-            _ => assert!(1 == 0, "Language not recognized properly!"),
-        }
-    }
-}
-
-
-#[cfg(test)]
-#[test]
-fn regex_group_domain_extractor_test() {
-    let _ = env_logger::init();
-    let domain_correct = vec!(
-        "/home/user/domains/domain.tld.my.pl/public_html/file.php",
-        "/home/user2/domains/domena.pl/public_html/index.html",
-    );
-    let correct_results = vec!(
-        "domain.tld.my.pl",
-        "domena.pl"
-    );
-    for (file, res) in domain_correct.iter().zip(correct_results.iter()) {
-        let entry = structs::FileEntry {
-            owner: Owner {
-                origin: String::new(), /* XXX */
-                name: String::from("root"),
-                account_type: structs::AccountType::Admin,
-                .. Default::default()
-            },
-            path: file.to_string(),
-            size: 123,
-            mode: 0711 as u32,
-            modified: 1,
-            encoding: "UTF-8".to_string(),
-            .. Default::default()
-        };
-
-        let domain_from_path = Regex::new(r".*/domains/(.*)/public_html/.*").unwrap();
-        for _domain in domain_from_path.captures_iter(entry.path.as_str()) {
-            debug!("Domain: {:?}", _domain.at(1).unwrap());
-            assert!(_domain.at(1).unwrap() == res.to_string());
         }
     }
 }
