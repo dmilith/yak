407df90188e8b3de5c8fec38b8af931f18edbbb6
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-03-30 01:03:21 +0200

    Defined root_invalid_uuid()

--- src/base.rs
+++ src/base.rs
@@ -4,3 +4,8 @@ use uuid::Uuid;
 pub fn root_uuid() -> Uuid {
     /* root */ Uuid::parse_str("4b84d962-ff55-4913-94fa-b20db7e1d2da").unwrap()
 }
+
+
+pub fn root_invalid_uuid() -> Uuid {
+    /* root */ Uuid::parse_str("deadbeef-ff55-4913-94fa-000000000000").unwrap()
+}
