a0eb3911b0e6b24347fa428a537d754feae6095c
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-03-30 18:33:06 +0200

    imp: Filter newline characters from local_content

--- src/process.rs
+++ src/process.rs
@@ -204,7 +204,7 @@ pub fn process_file(abs_path: &str, f: &File) -> Result<FileEntry, String> {
                 let mut entry = FileEntry {
                     owner: an_owner,
                     path: abs_path.to_string(),
-                    local_content: binary_content.clone(),
+                    local_content: binary_content.clone().into_iter().filter(|e| *e == 10).collect::<Vec<u8>>(),
                     size: metadata.size(),
                     mode: metadata.mode() as u32,
                     modified: get_time().sec - metadata.mtime(),
