6ba26ff8ccc2b3c9274068fbc49d028a7156493a
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-04-04 20:03:27 +0200

    imp: Set html as default output format

--- src/web_panel.rs
+++ src/web_panel.rs
@@ -34,6 +34,7 @@ impl Handler for Api {
         response.headers_mut().set(AccessControlAllowOrigin::Any);
         response.headers_mut().set(AccessControlAllowMethods(methods));
         response.headers_mut().set(AccessControlAllowHeaders(vec![UniCase("content-type".into())]));
+        response.headers_mut().set(ContentType(content_type!(Text / Html; Charset = Utf8)));
 
         if let Some(action) = self.0 {
             action(context, response);
